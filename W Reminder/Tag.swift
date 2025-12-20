a8a1a515c450d9aebe4748a03f7d3b76ce2070e2
tysuprawee <ty.suprawee2929@gmail.com>
2025-12-19 22:44:03 -0500

    release 1.01

--- W Reminder/Tag.swift
+++ W Reminder/Tag.swift
@@ -16,6 +16,9 @@ final class Tag {
     // Inverse relationship (optional but good for cascade delete if needed)
     // @Relationship(deleteRule: .nullify, inverse: \Checklist.tag) var checklists: [Checklist]
 
+    @Relationship(inverse: \Checklist.tags) var checklists: [Checklist] = []
+    @Relationship(inverse: \SimpleChecklist.tags) var simpleChecklists: [SimpleChecklist] = []
+
     init(name: String, colorHex: String) {
         self.name = name
         self.colorHex = colorHex
