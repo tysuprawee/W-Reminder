c4a3a4ccf937cd893b44359a0f0dd51b23612bff
tysuprawee <ty.suprawee2929@gmail.com>
2025-12-21 02:26:16 -0500

    v1.03 init

---
+++ WReminderWidget/WReminderWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WReminderWidgetBundle.swift
+//  WReminderWidget
+//
+//  Created by Suprawee Pongpeeradech on 12/20/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WReminderWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WReminderWidget()
+        WReminderWidgetControl()
+    }
+}
